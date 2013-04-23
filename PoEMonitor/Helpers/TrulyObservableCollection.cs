27b851cfb3517d5221c561145781a90a24516988
Ralf Grubenmann <ralf.grubenmann+github@gmail.com>
2013-04-23 08:09:25 +0200

    Initial Commit

---
+++ PoEMonitor/Helpers/TrulyObservableCollection.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace PoEMonitor.Helpers
+{
+    public class TrulyObservableCollection<T> : ObservableCollection<T>
+where T : INotifyPropertyChanged
+    {
+        public TrulyObservableCollection()
+            : base()
+        {
+            CollectionChanged += new NotifyCollectionChangedEventHandler(TrulyObservableCollection_CollectionChanged);
+        }
+
+        void TrulyObservableCollection_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (Object item in e.NewItems)
+                {
+                    (item as INotifyPropertyChanged).PropertyChanged += new PropertyChangedEventHandler(item_PropertyChanged);
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (Object item in e.OldItems)
+                {
+                    (item as INotifyPropertyChanged).PropertyChanged -= new PropertyChangedEventHandler(item_PropertyChanged);
+                }
+            }
+        }
+
+        void item_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            NotifyCollectionChangedEventArgs a = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+            OnCollectionChanged(a);
+        }
+    }
+}
